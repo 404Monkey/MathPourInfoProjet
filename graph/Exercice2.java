495fc428ffb35dcafaad27bdfca313cbf636f540
Samuel <61194052+Strikza@users.noreply.github.com>
2021-01-07 12:13:17 +0100

    Exercice 2

--- graph/Exercice2.java
+++ graph/Exercice2.java
@@ -4,7 +4,23 @@ public class Exercice2 {
 
     private boolean isConnex;
 
-    public static void testConnex(GraphSimple graph) {
+    public Exercice2(){
+        this.isConnex = true;
+    }
+
+    public void testConnex(GraphSimple src) {
+        Exercice1 parcours = new Exercice1();
+
+        parcours.parcoursLargeur(1, src);
+
+        for(int x=1; x <= src.order(); x++){
+            if(parcours.getColor(x).equals(Exercice1.Color.GREEN)){
+                this.isConnex = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         //TODO
     }
 }
