985d6f90f0137a12bb718adf245d0d285bfec56a
Benjamin <cbenjamin174@gmail.com>
2021-01-07 14:49:01 +0100

    fin exo1

--- graph/Exercice1.java
+++ graph/Exercice1.java
@@ -39,6 +39,24 @@ public class Exercice1 {
         this.parent[x-1] = par;
     }
 
+    public static void displayColorArray(Color[] c){
+        for(int i = 0; i < c.length; i++){
+            if(c[i].equals(Color.GREEN)) {
+                System.out.print(c[i]+" ");
+            }
+            if(c[i].equals(Color.RED)) {
+                System.out.print(c[i]+"   ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void displayIntArray(int[] t){
+        for(int i = 0; i < t.length; i++){
+            System.out.print(t[i]+"     ");
+        }
+        System.out.println();
+    }
 
     //Question c
     public void initColor(){
@@ -68,7 +86,6 @@ public class Exercice1 {
                 if(this.getColor(y).equals(Color.GREEN)){
                     changeValue(y, getDistance(x)+1, Color.ORANGE, x);
                     F.add(y);
-                    System.out.println(F);
                 }
             }
             F.remove(0);
@@ -106,23 +123,56 @@ public class Exercice1 {
 
     //Question e et f
     public static void main(String[] args) {
-        //TODO
-        // penser à utliser GraphSimpleIO (GraphSimpleIO.getMatrix() ou dans exo1.main)
-        GraphSimpleIO.InitializeSource("graph-003.alists");
+        // TEST POUR GRAPH DE PETERSON
+
+        // On initialise un scanner dans le fichier du graphe de peterson
+        GraphSimpleIO.InitializeSource("graph-002.alists");
+        
+        // On récupère sa taille
         int n = GraphSimpleIO.scan.nextInt();
 
-        GraphSimple g = new GraphSimple(n);
+        // On crée un objet GraphSimple pour le graphe de peterson
+        GraphSimple peterson = new GraphSimple(n);
         
-        int[][] ng = GraphSimpleIO.getGraph(g.matrix);
-        g.matrix = ng;
-        GraphSimpleIO.printGraph(g.matrix);
+        // on récupère grace à getGraph les listes d'adjacences sans les somments au début et les 0 à la fin
+        int[][] agraph = GraphSimpleIO.getGraph(peterson.matrix);
+        peterson.matrix = agraph;
+        // On l'affiche pour vérifier
+        GraphSimpleIO.printGraph(peterson.matrix);
 
+        // On crée une classe Exercice1 puis on lui applique la méthode parcours largeur
         Exercice1 graph = new Exercice1();
-        graph.parcoursLargeur(g);
-        for(int i =0; i < graph.color.length; i++){
-            System.out.println(graph.color[i]);
-            System.out.println(graph.distance[i]);
-            System.out.println(graph.parent[i]);
-        }
+        graph.parcoursLargeur(1, peterson);
+
+        // On affiche les 3 tableaux
+        displayColorArray(graph.color);
+        displayIntArray(graph.distance);
+        displayIntArray(graph.parent);
+
+        // TEST POUR GRAPH 003
+
+        // On initialise un scanner dans le fichier du graphe de graph_003
+        GraphSimpleIO.InitializeSource("graph-003.alists");
+        
+        // On récupère sa taille
+        int n2 = GraphSimpleIO.scan.nextInt();
+
+        // On crée un objet GraphSimple pour le graphe de graph_003
+        GraphSimple graph_003 = new GraphSimple(n2);
+        
+        // on récupère grace à getGraph les listes d'adjacences sans les somments au début et les 0 à la fin
+        int[][] agraph_003 = GraphSimpleIO.getGraph(graph_003.matrix);
+        graph_003.matrix = agraph_003;
+        // On l'affiche pour vérifier
+        GraphSimpleIO.printGraph(graph_003.matrix);
+
+        // On crée une classe Exercice1 puis on lui applique la méthode parcours largeur
+        Exercice1 graph2 = new Exercice1();
+        graph2.parcoursLargeur(graph_003);
+
+        // On affiche les 3 tableaux
+        displayColorArray(graph2.color);
+        displayIntArray(graph2.distance);
+        displayIntArray(graph2.parent);
     }
 }
